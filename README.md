2380fc4963475fe73fe632fcdc25a117f81da0b4
Karthik Jagadeesha <kartu8@gmail.com>
2016-07-18 15:14:21 +0530

    changed for loop and edited readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # combinations.js
 This app finds all the possible character arrangements in a given string
 
-Works only for non repeatative characters
+Works only for non repeatative characters and below 5 character length 
--- combinations.js
+++ combinations.js
@@ -1,40 +1,40 @@
 const combinations = input => {
 
-  input = input.split("")
+    input = input.split("")
 
-  let patterns = []
-  let originalInput = input.slice(0)
-  let counter = 0
+    let patterns = []
+    let originalInput = input.slice(0)
+    let counter = 0
 
-  const patternRepeated = pattern => patterns.some(characters => characters === pattern)
+    const patternRepeated = pattern => patterns.some(characters => characters === pattern)
 
-  for(let i = 0; i < input.length; i++) {
-    input = originalInput.slice(0)
-    let firstCharacter = input[i]
-    input.splice(i, 1)
-    input.unshift(firstCharacter)
+    for(let i = 0; i < input.length; i++) {
+        input = originalInput.slice(0)
+        let firstCharacter = input[i]
+        input.splice(i, 1)
+        input.unshift(firstCharacter)
 
-    top: for(let j = 0; ; j++) {
-      let currentCharacter = input[input.length - 1]
+        top: for( ; ; ) {
+            let currentCharacter = input[input.length - 1]
 
-      while(input.indexOf(currentCharacter) !== 1) {
-        let tempIndex = input.indexOf(currentCharacter)
-        input.splice(input.indexOf(currentCharacter) - 1, 0, currentCharacter)
-        input.splice(tempIndex + 1, 1)
+            while(input.indexOf(currentCharacter) !== 1) {
+                let tempIndex = input.indexOf(currentCharacter)
+                input.splice(input.indexOf(currentCharacter) - 1, 0, currentCharacter)
+                input.splice(tempIndex + 1, 1)
 
-        if(patternRepeated(input.join(""))) {
-          break top
+                if(patternRepeated(input.join(""))) {
+                    break top
+                }
+                else {
+                    patterns.push(input.join(""))
+                    counter++
+                }
+
+                console.log(counter + ": " + input.join(""))
+            }
         }
-        else {
-          patterns.push(input.join(""))
-          counter++
-        }
-
-        console.log(counter + ": " + input.join(""))
-      }
     }
-  }
-  console.log("\nThere are a total of " + counter + " combinations")
+    console.log("\nThere are a total of " + counter + " combinations")
 }
 
-combinations("star")
+combinations("staredom")
